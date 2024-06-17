2b76d361abe8a4adb5d51a399759930e3bd8cd90
AdityaKumar41 <119885098+AdityaKumar41@users.noreply.github.com>
2024-06-17 17:41:41 +0530

    change uri

--- controller/urlcontroler.js
+++ controller/urlcontroler.js
@@ -1,42 +1,54 @@
 const urlModel = require("../model/url");
 const ShortUniqueId = require("short-unique-id");
 async function handleUserUrl(req, res) {
-  const uid = req.params.shortId;
-  const entry = await urlModel.findOneAndUpdate(
-    { urlEncoded: uid },
-    {
-      $push: {
-        visitHistory: { timestamp: Date.now(), Ipaddress: req.ip },
-      },
-    }
-  );
-  res.redirect(entry.redirectUrl);
+  try {
+    const uid = req.params.shortId;
+    const entry = await urlModel.findOneAndUpdate(
+      { urlEncoded: uid },
+      {
+        $push: {
+          visitHistory: { timestamp: Date.now(), Ipaddress: req.ip },
+        },
+      }
+    );
+    return res.redirect(entry.redirectUrl);
+  } catch (e) {
+    return;
+  }
 }
 
 async function handleCreateUrl(req, res) {
-  const body = req.body;
-  console.log(body);
-  if (!body.url) {
-    return res.status(400).send({ status: "Url Required" });
+  try {
+    const body = req.body;
+    console.log(body);
+    if (!body.url) {
+      return res.status(400).send({ status: "Url Required" });
+    }
+    const uid = new ShortUniqueId();
+    const idgen = uid.rnd(12);
+    const result = await urlModel.create({
+      urlEncoded: idgen,
+      redirectUrl: body.url,
+      visitHistory: [],
+    });
+    const getUser = await urlModel.findOne({ urlEncoded: idgen });
+    return res.status(200).json({ shortUrl: getUser });
+  } catch (e) {
+    return;
   }
-  const uid = new ShortUniqueId();
-  const idgen = uid.rnd(12);
-  const result = await urlModel.create({
-    urlEncoded: idgen,
-    redirectUrl: body.url,
-    visitHistory: [],
-  });
-  const getUser = await urlModel.findOne({ urlEncoded: idgen });
-  return res.status(200).json({ shortUrl: getUser });
 }
 
 async function handleHistory(req, res) {
-  const uid = req.params.shortId;
-  const result = await urlModel.findOne({ urlEncoded: uid });
-  return res.json({
-    totalclicks: result.visitHistory.length,
-    analytics: result.visitHistory,
-  });
+  try {
+    const uid = req.params.shortId;
+    const result = await urlModel.findOne({ urlEncoded: uid });
+    return res.json({
+      totalclicks: result.visitHistory.length,
+      analytics: result.visitHistory,
+    });
+  } catch (e) {
+    return;
+  }
 }
 
 module.exports = {
